874a693630e4a44b1bb16bdc479392efb0983aab
Melvin Keskin <melvo@olomono.de>
2022-04-10 16:31:39 +0200

    xmpp-providers: Add simple lists

--- xmpp-providers-docker/entrypoint.sh
+++ xmpp-providers-docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -xeo pipefail
 
 # Generate lists
-for list in A B C D; do
+for list in A B C D As Bs Cs Ds; do
 	FETCH_URL="https://invent.kde.org/melvo/xmpp-providers/-/jobs/artifacts/master/raw/providers-${list}.json?job=filtered-provider-lists"
 	curl -L -o "/var/www/public/providers-${list}.json" "$FETCH_URL"
 done
