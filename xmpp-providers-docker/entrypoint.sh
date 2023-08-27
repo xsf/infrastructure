ebc8c4895152c320407f40c3a868af7d2721ed21
Melvin Keskin <melvo@olomono.de>
2023-08-27 16:01:14 +0200

    entrypoint: Fetch providers file and improve comments

--- xmpp-providers-docker/entrypoint.sh
+++ xmpp-providers-docker/entrypoint.sh
@@ -2,11 +2,14 @@
 
 set -xeo pipefail
 
-# Generate lists
+# Fetch the original providers file.
+curl -L -o "/var/www/public/providers.json" "https://invent.kde.org/melvo/xmpp-providers/-/jobs/artifacts/master/raw/providers.json?job=providers-file"
+
+# Fetch the filtered provider lists.
 for list in A B C D As Bs Cs Ds; do
 	FETCH_URL="https://invent.kde.org/melvo/xmpp-providers/-/jobs/artifacts/master/raw/providers-${list}.json?job=filtered-provider-lists"
 	curl -L -o "/var/www/public/providers-${list}.json" "$FETCH_URL"
 done
 
-# Sleep 1 hour before refreshing
+# Wait 1 hour before fetching the latest files again.
 exec /bin/sleep 3600
